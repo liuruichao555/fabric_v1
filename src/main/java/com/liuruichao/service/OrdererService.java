68bb6595b98fcab0ee4a15a0f025e1ae42753a5f
liuruichao <liuruichao555@126.com>
2017-03-24 17:33:58 +0800

    整理代理

--- src/main/java/com/liuruichao/service/OrdererService.java
+++ src/main/java/com/liuruichao/service/OrdererService.java
@@ -2,7 +2,6 @@ package com.liuruichao.service;
 
 import com.liuruichao.model.Member;
 import com.liuruichao.sdk.Chain;
-import com.liuruichao.security.CryptoPrimitive;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -14,18 +13,14 @@ import org.junit.Test;
  */
 @Slf4j
 public class OrdererService {
-    private String lrcBlockPath = "/Users/liuruichao/javaSRC/oxchains/fabric_v1/src/main/resources/foo.configtx";
-
-    private String name = "foo";
-
     private MemberService memberService = new MemberService();
-
-    private CryptoPrimitive cryptoPrimitive = new CryptoPrimitive();
 
     @Test
     public void testCreateChain() throws Exception {
+        String name = "foo";
+        String configtxPath = "/Users/liuruichao/javaSRC/oxchains/fabric_v1/src/main/resources/foo.configtx";
         Member member = memberService.enroll("admin", "adminpw");
-        Chain chain = new Chain(name, "localhost", 7050, lrcBlockPath, member);
+        Chain chain = new Chain(name, "localhost", 7050, configtxPath, member);
         System.out.println(chain.getGenesisBlock());
     }
 }
