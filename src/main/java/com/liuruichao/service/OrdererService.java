e8a5bc1d958692e8295b785c4e7ff93a5c16375b
liuruichao <liuruichao555@126.com>
2017-03-27 15:03:33 +0800

    change pom.xml

--- src/main/java/com/liuruichao/service/OrdererService.java
+++ src/main/java/com/liuruichao/service/OrdererService.java
@@ -3,6 +3,7 @@ package com.liuruichao.service;
 import com.liuruichao.model.Member;
 import com.liuruichao.sdk.Chain;
 import lombok.extern.slf4j.Slf4j;
+import org.hyperledger.protos.Chaincode;
 import org.junit.Test;
 
 /**
@@ -13,6 +14,10 @@ import org.junit.Test;
  */
 @Slf4j
 public class OrdererService {
+    private static final String CHAIN_CODE_NAME = "example_cc.go";
+    private static final String CHAIN_CODE_PATH = "github.com/example_cc";
+    private static final String CHAIN_CODE_VERSION = "1.0";
+
     private MemberService memberService = new MemberService();
 
     @Test
@@ -22,5 +27,14 @@ public class OrdererService {
         Member member = memberService.enroll("admin", "adminpw");
         Chain chain = new Chain(name, "localhost", 7050, configtxPath, member);
         System.out.println(chain.getGenesisBlock());
+        Chaincode.ChaincodeID chaincodeID = Chaincode.ChaincodeID.newBuilder()
+                .setName(CHAIN_CODE_NAME)
+                .setVersion(CHAIN_CODE_VERSION)
+                .setPath(CHAIN_CODE_PATH).build();
+
+    }
+
+    private void genInstallProposalRequest() {
+        // TODO 生成安装chaincode请求
     }
 }
