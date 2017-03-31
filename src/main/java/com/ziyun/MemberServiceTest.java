ed347111282148cf35b65761f28b3d80a4115607
liuruichao <liuruichao555@126.com>
2017-03-31 23:49:31 +0800

    add query method

--- src/main/java/com/ziyun/MemberServiceTest.java
+++ src/main/java/com/ziyun/MemberServiceTest.java
@@ -3,6 +3,8 @@ package com.ziyun;
 import com.ziyun.model.Customer;
 import com.ziyun.util.CryptoUtils;
 import org.hyperledger.fabric.sdk.*;
+import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
+import org.hyperledger.fabric.sdk.exception.ProposalException;
 import org.hyperledger.fabric_ca.sdk.HFCAClient;
 
 import java.util.ArrayList;
@@ -16,12 +18,18 @@ import java.util.Properties;
  * Created on 2017/3/29 21:54
  */
 public class MemberServiceTest {
+    private static final String CHAIN_CODE_NAME = "example_cc.go";
+
+    private static final String CHAIN_CODE_PATH = "github.com/example_cc";
+
+    private static final String CHAIN_CODE_VERSION = "1.0";
+
     public static void main(String[] args) throws Exception {
         String username = "admin";
         ArrayList<String> roles = new ArrayList<>();
         String account = "ziyun";
         String affiliation = "affiliation";
-        String mspID = "Org0MSP";
+        String mspID = "Org1MSP";
         //String mspID = "ziyun.MSP";
 
         MemberServices memberServices = new HFCAClient("http://localhost:7054", null);
@@ -40,13 +48,31 @@ public class MemberServiceTest {
         orderProperties.setProperty("pemFile", pemPath);
         orderProperties.setProperty("trustServerCertificate", "true");*/
 
-        Orderer orderer = hfClient.newOrderer("mychannel", "grpc://localhost:7050", orderProperties);
+        Orderer orderer = hfClient.newOrderer("foo", "grpc://localhost:7050", orderProperties);
         Peer peer = hfClient.newPeer("peer0", "grpc://localhost:7051");
-        Chain chain = hfClient.newChain("mychannel");
+        Chain chain = hfClient.newChain("foo");
         chain.addOrderer(orderer);
-        chain.joinPeer(peer);
+        chain.addPeer(peer);
         chain.initialize();
-        Collection<Peer> peers = chain.getPeers();
-        System.out.println(peers);
+
+        //chain.addPeer(peer);
+        //chain.initialize();
+        //Collection<Peer> peers = chain.getPeers();
+        //System.out.println(peers);
+    }
+
+    private void query(HFClient hfClient, Chain chain) throws ProposalException, InvalidArgumentException {
+        ChainCodeID chainCodeID = ChainCodeID.newBuilder().setName(CHAIN_CODE_NAME)
+                .setVersion(CHAIN_CODE_VERSION).build();
+        // .setPath(CHAIN_CODE_PATH).build(); // 查询不需要path
+        QueryProposalRequest queryProposalRequest = hfClient.newQueryProposalRequest();
+        queryProposalRequest.setArgs(new String[]{"query", "b"});
+        queryProposalRequest.setFcn("invoke");
+        queryProposalRequest.setChaincodeID(chainCodeID);
+
+        Collection<ProposalResponse> queryProposals = chain.sendQueryProposal(queryProposalRequest, chain.getPeers());
+        for (ProposalResponse response : queryProposals) {
+            System.out.println(response.getProposalResponse().getResponse().getPayload().toStringUtf8());
+        }
     }
 }
